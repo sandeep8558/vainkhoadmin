94b5851410df960b4f9780c115c7ba47fb1db434
sandeep8558 <sandeep198558@gmail.com>
2023-05-12 04:18:56 +0530

    First Upload

--- public/js/app.js
+++ public/js/app.js
@@ -53030,8 +53030,8 @@ $(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Projects\NEW\useless\useless\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Projects\NEW\useless\useless\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Projects\NEW\Vainkho\admin\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Projects\NEW\Vainkho\admin\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
